bfadfb73279fb89e6b0cb6ad8923b43939f2c20c
Toby Chow <tobychow98@gmail.com>
2019-06-10 12:05:35 -0500

    Clicking square will fill

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ class Square extends React.Component {
       return (
         <button 
           className="square" 
-          onClick={() => this.setState({value: 'X'})} // will set state to X after button is clicked. will rerender square
+          onClick={() => this.props.onClick()} // will set state to X after button is clicked. will rerender square
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
@@ -29,8 +29,20 @@ class Square extends React.Component {
         squares: Array(9).fill(null),
       };
     }
+
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-      return <Square value={this.state.squares[i]} />;
+      return (
+        <Square 
+          value={this.state.squares[i]} 
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
